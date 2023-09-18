680233ad2d98343534d41ce942624f9d527eb61e
Usman <usman.mughal@outlook.fr>
2023-09-18 18:00:12 +0200

    màj navbar

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css";
+
+function Navbar() {
+  return <nav className="navbar">
+    <div className ="navbar_logo">
+      <img src="logo.png" alt="Logo" />
+    </div>
+    <div>Acceuil</div>
+    <div>A propos</div>
+  </nav>;
+}
+  
+export default Navbar;
