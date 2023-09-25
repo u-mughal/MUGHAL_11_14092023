14ffbddabf6e8586e83de4e03b35e099dd9e9e12
Usman <usman.mughal@outlook.fr>
2023-09-25 15:14:27 +0200

    install react router dom + add fichier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Apropos from "./pages/Apropos";
+import Home from "./pages/Home";
+import Page404 from "./pages/Page404";
+import Pagelogements from "./pages/Pagelogements";
+
 const App = () => {
-  return <h1>Hello react</h1>;
-} 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Apropos" element={<Apropos />} />
+        <Route path="/*" element={<Page404 />} />
+        <Route path="/Logement" element={<Pagelogements />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/pages/Pagelogements.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pagelogements = () => {
+  return (
+    <div>
+      <h1>logements</h1>
+    </div>
+  );
+};
+
+export default Pagelogements;
