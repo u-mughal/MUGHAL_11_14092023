abf3cbf24c86576aa1d8e63ce4abe3920a1fa05a
Usman <usman.mughal@outlook.fr>
2023-10-18 12:25:33 +0200

    màj Service.jsx, Apropos.jsx et Dropdown.css

--- src/_Services/Logement.service.jsx
+++ src/_Services/Logement.service.jsx
@@ -1,16 +1,22 @@
-import logements from "@/Assets/Api/Logement.json";
+/** Import des modules nécessaires */
+import ListeLogements from "@/Assets/Api/Logement.json";
 
+// fonction récuperation de tous les logements.
 let GetAllLogement = () => {
-    return logements;
+    return ListeLogements;
 }
 
-let GetLogementById = (id) => {
-    return logements.find(logement => logement.id === id);
+// fonction recupération d'un logement.
+let GetOneLogement = async (id) => {
+    const OneLogement = await ListeLogements.find(logement => logement.id === id);
+    return OneLogement;
 }
 
+// export des fonction pour les reutiliser dans les pages
 export const LogementService = {
     GetAllLogement,
-    GetLogementById
-};
+    GetOneLogement
+}
 
-export default LogementService;
+
+export default LogementService
