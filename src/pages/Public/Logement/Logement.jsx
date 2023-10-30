ee9ff3dcb0a64800b54fd61ce8af7c0b05d89c99
Usman <usman.mughal@outlook.fr>
2023-10-30 16:24:51 +0100

    Logement Page, Component Rating, Tag

--- src/pages/Public/Logement/Logement.jsx
+++ src/pages/Public/Logement/Logement.jsx
@@ -1,50 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import '@/Pages/Public/Logement/Logement.css';
 import LogementService from '@/_Services/Logement.service.jsx';
-import DropdownMenu from '@/Components/Dropdown/Dropdown';
-import Carrousel from '@/Components/Carrousel/Carrousel';
-
-
-import './Logement.css'
+import Carrousel from '@/Components/Carrousel/Carrousel.jsx';
+import Tag from '@/Components/Tag/Tag';
+import Dropdown from '@/Components/Dropdown/Dropdown';
+import Rating from '@/Components/Rating/Rating';
 
 const Logement = () => {
-    const id = useParams().id;
-    const [logementInfo, setLogementInfo] = useState();
+    const { id } = useParams();
     const navigate = useNavigate();
+    const [logement, setLogement] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const fetchLogement = async () => {
-            try {
-                const logementData = await LogementService.GetOneLogement(id);
-                setLogementInfo(logementData);
-            } catch (error) {
-                console.log("An error occured while trying to retrive logementInfo", error);
-                navigate('/error');
-            }
-        }
-        fetchLogement();
+        LogementService.getLogementById(id)
+            .then(data => {
+                setLogement(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error(error);
+                navigate("/404");
+            });
     }, [id, navigate]);
 
-    if (!logementInfo) {
-        return null; //Il faudra voir la necessité de placé un loading spinner ici au cas ou.
+    if (loading) {
+        return <div>Chargement...</div>;
     }
 
     return (
-        <main className='MainLogement'>
-            <>
-                <section className='Carousel'>
-                    <Carrousel pictures={logementInfo.pictures} />
-                </section>
-                <section className='Logement'>
-                    <Logement logementInfo={logementInfo} />
-                </section>
-                <section className='Dropdown'>
-                    <DropdownMenu titre="Description" data={logementInfo.description} />
-                    <DropdownMenu titre="Équipements" data={logementInfo.equipments} />
-                </section>
-            </>
-        </main>
-    )
+        <section className="logement">
+            <Carrousel slides={logement.pictures} />
+            <div className="logement__content">
+                <div className="Container_1">
+                    <div className="Div__title-loc-tag">
+                        <h1 className="logement__title">{logement.title}</h1>
+                        <p className="logement__location">{logement.location}</p>
+                        <div className="TLC_tag-row">
+                            <Tag tags={logement.tags} />
+                        </div>
+                    </div>
+                    <div className="Div__Host-rating">
+                        <div className="Host">
+                            <h2 className="Host__name">{logement.host.name}</h2>
+                            <img className="Host__avatar" src={logement.host.picture} alt={logement.host.name} />
+                        </div>
+                    
+                        <Rating rating={logement.rating} />
+                        
+                    </div>
+                </div>
+                <div className="Container_2">
+                    <div className="logement__description">
+                        <Dropdown title="Description" content={logement.description} />
+                    </div>
+                    <div className="logement__equipements">
+                        <Dropdown title="Equipements" content={logement.equipments} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
-export default Logement.css;
+export default Logement;
